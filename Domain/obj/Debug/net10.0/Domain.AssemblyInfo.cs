e2749b2cad97a8486fa0f010dd3e7360093dc35d
r.sallam <r.sallam.c@nhc.sa>
2025-12-28 17:16:54 +0300

    dev

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e01029e71fdd651fd400b08e9b4339f2427a7b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17becdf121dd610ecd3a6564dcaacf0b020d134a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
