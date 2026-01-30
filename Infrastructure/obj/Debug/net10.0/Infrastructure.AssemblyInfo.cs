b85430e18bd5ae00eede9c4281715fb1fefd4921
moham <moham@LAPTOP-9EC0E8LR>
2026-01-30 21:31:11 +0300

    dev

--- Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3ea4ffed6010799e29161987192e6f6f2c0febf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5fe3ccd2a3e181b99cd9d46821e8519a40f39df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
